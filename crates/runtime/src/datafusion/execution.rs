bdade24bd9864f11267b81c834fe4a91b36a5a42
Scott Lyons <scottalyons@gmail.com>
2025-01-13 20:59:26 -0800

    Renaming fixes

--- crates/runtime/src/datafusion/execution.rs
+++ crates/runtime/src/datafusion/execution.rs
@@ -223,20 +223,20 @@ impl SqlExecutor {
         &self,
         query: &str,
         warehouse_name: &str,
-    ) -> SQLResult<Vec<RecordBatch>> {
+    ) -> IcehutSQLResult<Vec<RecordBatch>> {
         let plan = self.get_custom_logical_plan(query, warehouse_name).await?;
         let transformed = plan
             .transform(iceberg_transform)
             .data()
-            .context(sql_error::DataFusionSnafu)?;
+            .context(ih_error::DataFusionSnafu)?;
         let res = self
             .ctx
             .execute_logical_plan(transformed)
             .await
-            .context(sql_error::DataFusionSnafu)?
+            .context(ih_error::DataFusionSnafu)?
             .collect()
             .await
-            .context(sql_error::DataFusionSnafu)?;
+            .context(ih_error::DataFusionSnafu)?;
         Ok(res)
     }
 
