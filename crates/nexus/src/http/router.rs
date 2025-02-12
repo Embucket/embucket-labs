ec6bf2795f11a789bddbc2cc84d74c03d18c64a1
Scott Lyons <scottalyons@gmail.com>
2025-02-11 19:17:29 -0800

    Formatting

--- crates/nexus/src/http/router.rs
+++ crates/nexus/src/http/router.rs
@@ -66,7 +66,7 @@ pub fn create_app(state: AppState) -> Router {
                 .url("/ui_openapi.json", ui_spec),
         )
         .route("/health", get(|| async { Json("OK") }))
-        .route("/telemetry/send", post(|| async { Json("OK")}))
+        .route("/telemetry/send", post(|| async { Json("OK") }))
         .layer(TimeoutLayer::new(std::time::Duration::from_secs(1200)))
         .layer(CatchPanicLayer::new())
         .layer(
