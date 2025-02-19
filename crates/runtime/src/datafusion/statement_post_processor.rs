0d79e53208dd24bd01eb81cf6e75e09c035563f6
DanCodedThis <daniil.vysotskyi@gmail.com>
2025-02-19 18:28:35 +0200

    no macro, just simple vistor mut

--- crates/runtime/src/datafusion/statement_post_processor.rs
+++ crates/runtime/src/datafusion/statement_post_processor.rs
@@ -1,21 +1,22 @@
 use std::ops::ControlFlow;
 use datafusion::logical_expr::sqlparser::ast::{VisitorMut, Expr};
 use datafusion_expr::ExprFunctionExt;
-use sqlparser::ast::Function;
+use sqlparser::ast::{Function, FunctionArg, FunctionArgExpr, FunctionArgumentList, FunctionArguments, Ident, ObjectName};
 
 struct StatementPostProcessor;
-
-impl StatementPostProcessor {
-    fn yes<T>(value: Expr) {
-        matches!(value, T);
-    }
-}
 
 impl VisitorMut for StatementPostProcessor {
     type Break = ();
     fn post_visit_expr(&mut self, expr: &mut Expr) -> ControlFlow<Self::Break> {
-        if let Expr::Function(Function { name, args, .. }) = expr {
-            
+        if let Expr::Function(Function { name: ObjectName(idents), args: FunctionArguments::List(FunctionArgumentList { args, .. }), .. }) = expr  {
+            match idents.first().unwrap().value.as_str() {
+                "dateadd" | "date_add" | "datediff" | "date_diff" => {
+                    if let FunctionArg::Unnamed(FunctionArgExpr::Expr(Expr::Identifier(Ident { value, .. }))) = args.iter_mut().next().unwrap() {
+                        *value = format!("'{value}'");
+                    }
+                }
+                _ => {}
+            }
         }
         ControlFlow::Continue(())
     }
