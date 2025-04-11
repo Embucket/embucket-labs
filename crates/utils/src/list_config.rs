88f2e438f2e267102b6779b65e1eae8397886f36
DanCodedThis <daniil.vysotskyi@gmail.com>
2025-04-11 22:03:17 +0300

    minor comment fixes

--- crates/utils/src/list_config.rs
+++ crates/utils/src/list_config.rs
@@ -16,7 +16,7 @@
 // under the License.
 
 pub struct ListConfig {
-    //from where to start the scan range for SlateDB
+    //From where to start the scan range for SlateDB
     // ex: if we ended on "tested2", the cursor would be "tested2"
     // and inside the `fn list_objets` in utils crate the start of the range would be "tested2\x00"
     // ("\x00" is the smallest ASCII char to find anything after the "tested2" excluding it)
@@ -24,9 +24,9 @@ pub struct ListConfig {
     // (`\x7F` is the largest ASCII char to find anything before it)
     // if there are 4 tables `tested1..tested4` which would yield us "tested3" and "tested4" including other names if any exist
     pub cursor: Option<String>,
-    // limit of how much items to take back from `fn list_objets` in utils crate
+    //Limit of how much items to take back from `fn list_objets` in utils crate
     pub limit: Option<usize>,
-    //search string, from where (and to where in lexicographical sort order) to do the search
+    //Search string, from where (and to where in lexicographical sort order) to do the search
     // ex: if we want to find all the test tables it could be "tes" (if there are 4 tables `tested1..tested4`)
     // the range would be `tes..tes\x7F` tables
     // (`\x7F` is the largest ASCII char to find anything before it)
