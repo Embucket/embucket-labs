4d875a936a972e64db02af34c6c10be0765d9dd0
DanCodedThis <daniil.vysotskyi@gmail.com>
2025-04-28 16:58:56 +0300

    WIP

--- crates/runtime/src/http/ui/databases/error.rs
+++ crates/runtime/src/http/ui/databases/error.rs
@@ -5,6 +5,7 @@ use axum::Json;
 use embucket_metastore::error::MetastoreError;
 use http::StatusCode;
 use snafu::prelude::*;
+use crate::execution::error::ExecutionError;
 
 pub type DatabasesResult<T> = Result<T, DatabasesAPIError>;
 
@@ -21,6 +22,8 @@ pub enum DatabasesAPIError {
     Update { source: MetastoreError },
     #[snafu(display("Get databases error: {source}"))]
     List { source: MetastoreError },
+    #[snafu(display("Query engine error: {source}"))]   
+    Query { source: ExecutionError },
 }
 
 // Select which status code to return.
@@ -45,6 +48,7 @@ impl IntoStatusCode for DatabasesAPIError {
                 _ => StatusCode::INTERNAL_SERVER_ERROR,
             },
             Self::List { .. } => StatusCode::INTERNAL_SERVER_ERROR,
+            Self::Query { .. } => StatusCode::INTERNAL_SERVER_ERROR,       
         }
     }
 }
--- crates/runtime/src/http/ui/schemas/handlers.rs
+++ crates/runtime/src/http/ui/schemas/handlers.rs
@@ -1,10 +1,25 @@
-use crate::execution::query::QueryContext;
-use crate::http::session::DFSessionId;
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 use crate::http::state::AppState;
-use crate::http::ui::queries::models::ResultSet;
 use crate::http::ui::schemas::models::SchemasParameters;
 use crate::http::{
     error::ErrorResponse,
+    metastore::handlers::QueryParameters,
     ui::schemas::error::{SchemasAPIError, SchemasResult},
     ui::schemas::models::{
         Schema, SchemaCreatePayload, SchemaCreateResponse, SchemaResponse, SchemaUpdatePayload,
@@ -15,13 +30,16 @@ use axum::{
     extract::{Path, Query, State},
     Json,
 };
-use embucket_history::{QueryRecord, QueryRecordActions};
 use embucket_metastore::error::MetastoreError;
 use embucket_metastore::models::SchemaIdent as MetastoreSchemaIdent;
+use embucket_metastore::Schema as MetastoreSchema;
 use embucket_utils::scan_iterator::ScanIterator;
+use std::collections::HashMap;
 use std::convert::From;
 use std::convert::Into;
 use utoipa::OpenApi;
+use crate::execution::query::QueryContext;
+use crate::http::session::DFSessionId;
 
 #[derive(OpenApi)]
 #[openapi(
@@ -74,52 +92,15 @@ pub async fn create_schema(
         database: Some(database_name.clone()),
         schema: Some(payload.name.clone()),
     };
-    let sql_string = format!(
-        "CREATE SCHEMA {}.{}",
-        database_name.clone(),
-        payload.name.clone()
-    );
-    //TODO: figure out how to unify this, with the delete, instead of copying code (possibly a generic query history `fn`)
-    let mut query_record = QueryRecord::query_start(&sql_string, None);
-    let query_res = state
+    let sql_string = format!("CREATE SCHEMA {}.{}", database_name.clone(), payload.name.clone());
+    let _ = state
         .execution_svc
         .query(&session_id, sql_string.as_str(), context)
-        .await;
-    match query_res {
-        Ok((ref records, ref columns)) => {
-            let result_set = ResultSet::query_result_to_result_set(records, columns);
-            match result_set {
-                Ok(result_set) => {
-                    let encoded_res = serde_json::to_string(&result_set);
-
-                    if let Ok(encoded_res) = encoded_res {
-                        let result_count = i64::try_from(records.len()).unwrap_or(0);
-                        query_record.query_finished(result_count, Some(encoded_res));
-                    }
-                    // failed to wrap query results
-                    else if let Err(err) = encoded_res {
-                        query_record.query_finished_with_error(err.to_string());
-                    }
-                }
-                // error getting result_set
-                Err(err) => {
-                    query_record.query_finished_with_error(err.to_string());
-                }
-            }
-        }
-        // query error
-        Err(ref err) => {
-            // query execution error
-            query_record.query_finished_with_error(err.to_string());
-        }
-    }
-    if let Err(err) = query_res {
-        Err(SchemasAPIError::Create { source: err })
-    } else {
-        Ok(Json(SchemaCreateResponse {
-            data: Schema::new(payload.name, database_name),
-        }))
-    }
+        .await
+        .map_err(|e| SchemasAPIError::Query { source: e })?;
+    Ok(Json(SchemaCreateResponse {
+        data: Schema::new(payload.name, database_name),       
+    }))
 }
 
 #[utoipa::path(
@@ -139,58 +120,16 @@ pub async fn create_schema(
 )]
 #[tracing::instrument(level = "debug", skip(state), err, ret(level = tracing::Level::TRACE))]
 pub async fn delete_schema(
-    DFSessionId(session_id): DFSessionId,
     State(state): State<AppState>,
+    Query(query): Query<QueryParameters>,
     Path((database_name, schema_name)): Path<(String, String)>,
 ) -> SchemasResult<()> {
-    let context = QueryContext {
-        database: Some(database_name.clone()),
-        schema: Some(schema_name.clone()),
-    };
-    let sql_string = format!(
-        "DROP SCHEMA {}.{}",
-        database_name.clone(),
-        schema_name.clone()
-    );
-    //TODO: figure out how to unify this, with the create, instead of copying code (possibly a generic query history `fn`)
-    let mut query_record = QueryRecord::query_start(&sql_string, None);
-    let query_res = state
-        .execution_svc
-        .query(&session_id, sql_string.as_str(), context)
-        .await;
-    match query_res {
-        Ok((ref records, ref columns)) => {
-            let result_set = ResultSet::query_result_to_result_set(records, columns);
-            match result_set {
-                Ok(result_set) => {
-                    let encoded_res = serde_json::to_string(&result_set);
-
-                    if let Ok(encoded_res) = encoded_res {
-                        let result_count = i64::try_from(records.len()).unwrap_or(0);
-                        query_record.query_finished(result_count, Some(encoded_res));
-                    }
-                    // failed to wrap query results
-                    else if let Err(err) = encoded_res {
-                        query_record.query_finished_with_error(err.to_string());
-                    }
-                }
-                // error getting result_set
-                Err(err) => {
-                    query_record.query_finished_with_error(err.to_string());
-                }
-            }
-        }
-        // query error
-        Err(ref err) => {
-            // query execution error
-            query_record.query_finished_with_error(err.to_string());
-        }
-    }
-    if let Err(err) = query_res {
-        Err(SchemasAPIError::Delete { source: err })
-    } else {
-        Ok(())
-    }
+    let ident = MetastoreSchemaIdent::new(database_name, schema_name);
+    state
+        .metastore
+        .delete_schema(&ident, query.cascade.unwrap_or_default())
+        .await
+        .map_err(|e| SchemasAPIError::Delete { source: e })
 }
 
 #[utoipa::path(
