884de8676121bacb5018361ab7c4cc5c337206e8
Nikita Striuk <nikitastryuk@gmail.com>
2025-04-28 22:22:40 +0200

    test commit

--- ui/src/modules/query-history/query-history-page.tsx
+++ ui/src/modules/query-history/query-history-page.tsx
@@ -4,14 +4,17 @@ import { Input, InputIcon, InputRoot } from '@/components/ui/input';
 
 export function QueryHistoryPage() {
   return (
-    <div className="flex items-center justify-between border-b p-4">
-      <h1 className="text-xl font-semibold">Query History</h1>
-      <InputRoot>
-        <InputIcon>
-          <Search />
-        </InputIcon>
-        <Input className="min-w-80" disabled placeholder="Search" />
-      </InputRoot>
-    </div>
+    <>
+      <div className="flex items-center justify-between border-b p-4">
+        <h1 className="text-xl font-semibold">Query History</h1>
+        <InputRoot>
+          <InputIcon>
+            <Search />
+          </InputIcon>
+          <Input className="min-w-80" disabled placeholder="Search" />
+        </InputRoot>
+      </div>
+      <div className="p-4">Page content</div>
+    </>
   );
 }
