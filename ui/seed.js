545e7a510518fade69fea92a77e0789316a2d9bb
Nikita Striuk <nikitastryuk@gmail.com>
2025-05-13 18:24:41 +0200

    SqlEditorLeftPanelTrees default open

--- ui/seed.js
+++ ui/seed.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 
 const API_BASE_URL = 'http://localhost:3000';
 const ACCESS_TOKEN =
-  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImF1ZCI6IjEyNy4wLjAuMSIsImlhdCI6MTc0NzEzMTczNywiZXhwIjoxNzQ3MTMyNjM3fQ.JI57A0aQKlv76pTfRNsJTsqQqy7B2gVNDbHfbQs-jo4';
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJlbWJ1Y2tldCIsImF1ZCI6IjEyNy4wLjAuMSIsImlhdCI6MTc0NzE1MTE1OCwiZXhwIjoxNzQ3MTUyMDU4fQ.M-PKAmEftZJkWLRnGGSVzFvKL-N3SfS6b7sMkVLfciE';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
