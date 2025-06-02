2c4fdc0716b79e8ac64c95cc4e51883ebee50490
Maxim Bogdanov <muravlion@gmail.com>
2025-06-02 14:02:39 +0200

    rm is_*

--- crates/df-builtins/src/lib.rs
+++ crates/df-builtins/src/lib.rs
@@ -74,6 +74,7 @@ pub fn register_udfs(registry: &mut dyn FunctionRegistry) -> Result<()> {
         strtok_to_array::get_udf(),
         object_keys::get_udf(),
         try_parse_json::get_udf(),
+        typeof_func::get_udf(),
         Arc::new(ScalarUDF::from(ToBooleanFunc::new(false))),
         Arc::new(ScalarUDF::from(ToBooleanFunc::new(true))),
         Arc::new(ScalarUDF::from(ToTimeFunc::new(false))),
