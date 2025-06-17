0276299bc1949d903e99240af037e150bc047228
Denys Tsomenko <denistsem@gmail.com>
2025-06-17 17:44:16 +0300

    Clippy

--- crates/embucket-functions/src/visitors/top_limit.rs
+++ crates/embucket-functions/src/visitors/top_limit.rs
@@ -8,6 +8,41 @@ use std::ops::ControlFlow;
 pub struct TopLimitVisitor;
 
 impl TopLimitVisitor {
+    fn process_set_expr(&mut self, set_expr: &mut SetExpr, outer_limit: &mut Option<Expr>) {
+        match set_expr {
+            SetExpr::Select(select) => {
+                for table_with_joins in &mut select.from {
+                    if let TableFactor::Derived { subquery, .. } = &mut table_with_joins.relation {
+                        self.process_query(subquery);
+                    }
+                }
+
+                if let Some(top) = select.top.take() {
+                    if !top.percent && !top.with_ties {
+                        if outer_limit.is_none() {
+                            if let Some(expr) = top.quantity.map(|q| match q {
+                                TopQuantity::Expr(expr) => expr,
+                                TopQuantity::Constant(n) => Expr::Value(
+                                    Value::Number(n.to_string(), false).with_empty_span(),
+                                ),
+                            }) {
+                                *outer_limit = Some(expr);
+                            }
+                        }
+                    } else {
+                        select.top = Some(top);
+                    }
+                }
+            }
+            SetExpr::Query(q) => self.process_query(q),
+            SetExpr::SetOperation { left, right, .. } => {
+                self.process_set_expr(left, outer_limit);
+                self.process_set_expr(right, outer_limit);
+            }
+            _ => {}
+        }
+    }
+
     fn process_query(&mut self, query: &mut Query) {
         if let Some(with) = query.with.as_mut() {
             for cte in &mut with.cte_tables {
@@ -15,48 +50,7 @@ impl TopLimitVisitor {
             }
         }
 
-        fn process_set_expr(
-            this: &mut TopLimitVisitor,
-            set_expr: &mut SetExpr,
-            outer_limit: &mut Option<Expr>,
-        ) {
-            match set_expr {
-                SetExpr::Select(select) => {
-                    for table_with_joins in &mut select.from {
-                        if let TableFactor::Derived { subquery, .. } =
-                            &mut table_with_joins.relation
-                        {
-                            this.process_query(subquery);
-                        }
-                    }
-
-                    if let Some(top) = select.top.take() {
-                        if !top.percent && !top.with_ties {
-                            if outer_limit.is_none() {
-                                if let Some(expr) = top.quantity.map(|q| match q {
-                                    TopQuantity::Expr(expr) => expr,
-                                    TopQuantity::Constant(n) => Expr::Value(
-                                        Value::Number(n.to_string(), false).with_empty_span(),
-                                    ),
-                                }) {
-                                    *outer_limit = Some(expr);
-                                }
-                            }
-                        } else {
-                            select.top = Some(top);
-                        }
-                    }
-                }
-                SetExpr::Query(q) => this.process_query(q),
-                SetExpr::SetOperation { left, right, .. } => {
-                    process_set_expr(this, left, outer_limit);
-                    process_set_expr(this, right, outer_limit);
-                }
-                _ => {}
-            }
-        }
-
-        process_set_expr(self, &mut query.body, &mut query.limit);
+        self.process_set_expr(&mut query.body, &mut query.limit);
     }
 }
 
@@ -64,14 +58,13 @@ impl VisitorMut for TopLimitVisitor {
     type Break = ();
 
     fn pre_visit_statement(&mut self, stmt: &mut Statement) -> ControlFlow<Self::Break> {
-        match stmt {
-            Statement::Query(query) => self.process_query(query),
-            _ => {}
+        if let Statement::Query(query) = stmt {
+            self.process_query(query);
         }
         ControlFlow::Continue(())
     }
 }
 
 pub fn visit(stmt: &mut Statement) {
-    let _ = stmt.visit(&mut TopLimitVisitor::default());
+    let _ = stmt.visit(&mut TopLimitVisitor);
 }
