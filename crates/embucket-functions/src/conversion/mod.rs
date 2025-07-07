3153384c950ce916594f8f323420cef3d2f83a4d
DanCodedThis <daniil.vysotskyi@gmail.com>
2025-07-07 23:00:18 +0300

    mod

--- crates/embucket-functions/src/conversion/mod.rs
+++ crates/embucket-functions/src/conversion/mod.rs
@@ -12,6 +12,7 @@ use std::sync::Arc;
 pub use to_binary::ToBinaryFunc;
 pub use to_boolean::ToBooleanFunc;
 pub use to_time::ToTimeFunc;
+use crate::conversion::to_decimal::ToDecimalFunc;
 
 pub fn register_udfs(registry: &mut dyn FunctionRegistry) -> datafusion_common::Result<()> {
     let functions: Vec<Arc<ScalarUDF>> = vec![
@@ -23,6 +24,8 @@ pub fn register_udfs(registry: &mut dyn FunctionRegistry) -> datafusion_common::
         Arc::new(ScalarUDF::from(ToBooleanFunc::new(true))),
         Arc::new(ScalarUDF::from(ToTimeFunc::new(false))),
         Arc::new(ScalarUDF::from(ToTimeFunc::new(true))),
+        Arc::new(ScalarUDF::from(ToDecimalFunc::new(false))),
+        Arc::new(ScalarUDF::from(ToDecimalFunc::new(true))),
     ];
     for func in functions {
         registry.register_udf(func)?;
