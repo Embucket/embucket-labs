66a386cbf39c5f4cd9d435cfb20f03b8a7a8be5f
DanCodedThis <daniil.vysotskyi@gmail.com>
2025-07-08 22:14:20 +0300

    Simplifying signature to check @ return & @ invoke

--- crates/embucket-functions/src/conversion/to_decimal.rs
+++ crates/embucket-functions/src/conversion/to_decimal.rs
@@ -1,10 +1,10 @@
 use datafusion::arrow::datatypes::DataType;
 use datafusion::error::Result as DFResult;
 use datafusion::logical_expr::{Coercion, ColumnarValue, Signature, TypeSignature, TypeSignatureClass, Volatility};
-use datafusion_expr::{ScalarFunctionArgs, ScalarUDFImpl};
+use datafusion_expr::{ReturnInfo, ReturnTypeArgs, ScalarFunctionArgs, ScalarUDFImpl};
 use std::any::Any;
-use std::sync::Arc;
-use datafusion_common::types::{logical_float64, logical_int8, logical_string, logical_uint32, logical_uint8, NativeType};
+use datafusion_common::internal_err;
+use datafusion_common::types::{logical_float16, logical_float32, logical_float64, logical_int8, logical_string, logical_uint32, logical_uint8, NativeType};
 
 #[derive(Debug)]
 pub struct ToDecimalFunc {
@@ -25,64 +25,21 @@ impl ToDecimalFunc {
             signature: Signature::one_of(
                 vec![
                     //TO_DECIMAL( <expr> )
-                    TypeSignature::Coercible(vec![
-                        Self::expr_arg_type(),
-                    ]),
-                    //TO_DECIMAL( <expr> [, <precision> ] )
-                    TypeSignature::Coercible(vec![
-                        Self::expr_arg_type(),
-                        Self::precision_arg_type(),
-                    ]),
-                    //TO_DECIMAL( <expr> [, <precision> [, <scale> ] ] )
-                    TypeSignature::Coercible(vec![
-                        Self::expr_arg_type(),
-                        Self::precision_arg_type(),
-                        Self::scale_arg_type(),
-                    ]),
+                    TypeSignature::Any(1),
                     //TO_DECIMAL( <expr> [, '<format>' ] )
-                    TypeSignature::Coercible(vec![
-                        Self::expr_arg_type(),
-                        Self::format_arg_type(),
-                    ]),
+                    //TO_DECIMAL( <expr> [, <precision> ] )
+                    TypeSignature::Any(2),
                     //TO_DECIMAL( <expr> [, '<format>' ] [, <precision> ] )
-                    TypeSignature::Coercible(vec![
-                        Self::expr_arg_type(),
-                        Self::format_arg_type(),
-                        Self::precision_arg_type(),
-                    ]),
+                    //TO_DECIMAL( <expr> [, <precision> [, <scale> ] ] )
+                    TypeSignature::Any(3),
                     //TO_DECIMAL( <expr> [, '<format>' ] [, <precision> [, <scale> ] ] )
-                    TypeSignature::Coercible(vec![
-                        Self::expr_arg_type(),
-                        Self::format_arg_type(),
-                        Self::precision_arg_type(),
-                        Self::scale_arg_type(),
-                    ]),
+                    TypeSignature::Any(4),
                 ],
                 Volatility::Immutable,
             ),
             aliases: vec!["to_number".to_string(), "try_to_number".to_string(), "to_numeric".to_string(), "try_to_numeric".to_string()],
             r#try,
         }
-    }
-    fn expr_arg_type() -> Coercion {
-        //Needs numeric -> decimal type
-        Coercion::new_implicit(
-            TypeSignatureClass::Native(logical_string()),
-            vec![TypeSignatureClass::Integer, TypeSignatureClass::Native(logical_float64()), TypeSignatureClass::Native(logical_uint32())],
-            NativeType::String,
-        )
-    }
-
-    fn format_arg_type() -> Coercion {
-        Coercion::new_exact(TypeSignatureClass::Native(logical_string()))
-    }
-
-    fn precision_arg_type() -> Coercion {
-        Coercion::new_exact(TypeSignatureClass::Native(logical_uint8()))
-    }
-
-    fn scale_arg_type() -> Coercion {
-        Coercion::new_exact(TypeSignatureClass::Native(logical_int8()))
     }
 }
 
@@ -107,7 +64,12 @@ impl ScalarUDFImpl for ToDecimalFunc {
         &self.aliases
     }
 
-    fn return_type(&self, arg_types: &[DataType]) -> DFResult<DataType> {
+    fn return_type(&self, _arg_types: &[DataType]) -> DFResult<DataType> {
+        //TODO: Correct error type
+        internal_err!("return_type_from_args should be called")
+    }
+
+    fn return_type_from_args(&self, args: ReturnTypeArgs) -> DFResult<ReturnInfo> {
         todo!()
     }
 
