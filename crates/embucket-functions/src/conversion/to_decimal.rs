c09f1f45496cae3d872993b89c75044c32ca0dcf
DanCodedThis <daniil.vysotskyi@gmail.com>
2025-07-14 20:13:28 +0300

    some formatting to not fail slt tests

--- crates/embucket-functions/src/conversion/to_decimal.rs
+++ crates/embucket-functions/src/conversion/to_decimal.rs
@@ -1,14 +1,14 @@
-use datafusion::arrow::array::Decimal128Builder;
 use datafusion::arrow::compute::cast_with_options;
 use datafusion::arrow::datatypes::DataType;
 use datafusion::error::Result as DFResult;
 use datafusion::logical_expr::{ColumnarValue, Signature, TypeSignature, Volatility};
-use datafusion_common::arrow::array::{Array, Decimal128Array};
+use datafusion_common::arrow::array::{Array, StringArray};
 use datafusion_common::arrow::compute::CastOptions;
 use datafusion_common::arrow::util::display::FormatOptions;
 use datafusion_common::{ScalarValue, exec_err, internal_err};
 use datafusion_expr::{ReturnInfo, ReturnTypeArgs, ScalarFunctionArgs, ScalarUDFImpl};
 use std::any::Any;
+use std::fmt::Debug;
 use std::sync::Arc;
 
 #[derive(Debug)]
@@ -75,14 +75,14 @@ impl ScalarUDFImpl for ToDecimalFunc {
                 Some(
                     ScalarValue::Utf8(..) | ScalarValue::Utf8View(..) | ScalarValue::LargeUtf8(..),
                 ) => Ok(ReturnInfo::new(DataType::Decimal128(38, 0), true)),
-                Some(ScalarValue::Int64(Some(precession))) => {
-                    let Ok(precession) = u8::try_from(*precession) else {
+                Some(ScalarValue::Int64(Some(precision))) => {
+                    let Ok(precision) = u8::try_from(*precision) else {
                         return internal_err!(
-                            "invalid precession number (only allowed from 0 to 38): {}",
-                            precession
+                            "invalid precision number (only allowed from 0 to 38): {}",
+                            precision
                         );
                     };
-                    Ok(ReturnInfo::new(DataType::Decimal128(precession, 0), true))
+                    Ok(ReturnInfo::new(DataType::Decimal128(precision, 0), true))
                 }
                 other => {
                     internal_err!("unexpected data type: {:?}", other)
@@ -95,34 +95,34 @@ impl ScalarUDFImpl for ToDecimalFunc {
                         | ScalarValue::Utf8View(..)
                         | ScalarValue::LargeUtf8(..),
                     ),
-                    Some(ScalarValue::Int64(Some(precession))),
+                    Some(ScalarValue::Int64(Some(precision))),
                 ] => {
-                    let Ok(precession) = u8::try_from(*precession) else {
+                    let Ok(precision) = u8::try_from(*precision) else {
                         return internal_err!(
-                            "invalid precession number (only allowed from 0 to 38): {}",
-                            precession
+                            "invalid precision number (only allowed from 0 to 38): {}",
+                            precision
                         );
                     };
-                    Ok(ReturnInfo::new(DataType::Decimal128(precession, 0), true))
+                    Ok(ReturnInfo::new(DataType::Decimal128(precision, 0), true))
                 }
                 [
-                    Some(ScalarValue::Int64(Some(precession))),
+                    Some(ScalarValue::Int64(Some(precision))),
                     Some(ScalarValue::Int64(Some(scale))),
                 ] => {
-                    let Ok(precession) = u8::try_from(*precession) else {
+                    let Ok(precision) = u8::try_from(*precision) else {
                         return internal_err!(
-                            "invalid precession number (only allowed from 0 to 38): {}",
-                            precession
+                            "invalid precision number (only allowed from 0 to 38): {}",
+                            precision
                         );
                     };
                     let Ok(scale) = i8::try_from(*scale) else {
                         return internal_err!(
-                            "invalid precession number (only allowed from 0 to 1): {}",
+                            "invalid precision number (only allowed from 0 to <precision> - 1): {}",
                             scale
                         );
                     };
                     Ok(ReturnInfo::new(
-                        DataType::Decimal128(precession, scale),
+                        DataType::Decimal128(precision, scale),
                         true,
                     ))
                 }
@@ -133,23 +133,23 @@ impl ScalarUDFImpl for ToDecimalFunc {
             },
             4 => match &args.scalar_arguments[2..=3] {
                 [
-                    Some(ScalarValue::Int64(Some(precession))),
+                    Some(ScalarValue::Int64(Some(precision))),
                     Some(ScalarValue::Int64(Some(scale))),
                 ] => {
-                    let Ok(precession) = u8::try_from(*precession) else {
+                    let Ok(precision) = u8::try_from(*precision) else {
                         return internal_err!(
-                            "invalid precession number (only allowed from 0 to 38): {}",
-                            precession
+                            "invalid precision number (only allowed from 0 to 38): {}",
+                            precision
                         );
                     };
                     let Ok(scale) = i8::try_from(*scale) else {
                         return internal_err!(
-                            "invalid precession number (only allowed from 0 to 1): {}",
+                            "invalid precision number (only allowed from 0 to <precision> - 1): {}",
                             scale
                         );
                     };
                     Ok(ReturnInfo::new(
-                        DataType::Decimal128(precession, scale),
+                        DataType::Decimal128(precision, scale),
                         true,
                     ))
                 }
@@ -166,35 +166,90 @@ impl ScalarUDFImpl for ToDecimalFunc {
     //TODO: formatting <format> second arg
     #[allow(clippy::unwrap_used)]
     fn invoke_with_args(&self, args: ScalarFunctionArgs) -> DFResult<ColumnarValue> {
-        let DataType::Decimal128(precission, scale) = args.return_type else {
+        let DataType::Decimal128(precision, scale) = args.return_type else {
             return exec_err!("unexpected return type");
         };
 
         let expr = &args.args[0];
-
-        //let format = yadah yadah blah blah blah
-
         let array = match expr {
             ColumnarValue::Array(array) => array,
             ColumnarValue::Scalar(scalar) => &scalar.to_array()?,
         };
 
-        let mut result = Decimal128Builder::with_capacity(array.len())
-            .with_precision_and_scale(*precission, *scale)?;
+        let format = &args.args[1];
+        let format = match format {
+            ColumnarValue::Scalar(
+                ScalarValue::Utf8(Some(str))
+                | ScalarValue::Utf8View(Some(str))
+                | ScalarValue::LargeUtf8(Some(str)),
+            ) => Some(str.as_str()),
+            ColumnarValue::Scalar(
+                ScalarValue::Int64(Some(_))
+                | ScalarValue::Int32(Some(_))
+                | ScalarValue::Int16(Some(_))
+                | ScalarValue::Int8(Some(_))
+                | ScalarValue::UInt64(Some(_))
+                | ScalarValue::UInt32(Some(_))
+                | ScalarValue::UInt16(Some(_))
+                | ScalarValue::UInt8(Some(_))
+                | ScalarValue::Float64(Some(_))
+                | ScalarValue::Float32(Some(_)),
+            ) => None,
+            ColumnarValue::Array(_) | ColumnarValue::Scalar(_) => {
+                return exec_err!("unexpected second argument format");
+            }
+        };
 
-        match array.data_type() {
+        //TODO: should we have type info as before, good datapoint to think about on other types, functions, etc
+        let format_options = FormatOptions::default()
+            .with_null("NULL")
+            .with_types_info(false);
+
+        let result_array = match array.data_type() {
             DataType::Utf8 | DataType::Utf8View | DataType::LargeUtf8 => {
-                //TODO: needs logic for binary and varint types
-                let array = cast_with_options(
-                    array,
-                    &DataType::Decimal128(*precission, *scale),
+                let array = match format {
+                    Some(format) => {
+                        //TODO: needs logic for binary string with binary formatting and variant types
+                        let array: &StringArray = array.as_any().downcast_ref().unwrap();
+                        let values: Vec<_> = array.iter().collect();
+                        let values = if format.starts_with('$') {
+                            values
+                                .iter()
+                                .map(|opt| {
+                                    opt.map(|str| {
+                                        str.strip_prefix('$')
+                                            .map_or_else(|| str, |stripped| stripped)
+                                    })
+                                })
+                                .collect()
+                        } else {
+                            values
+                        };
+                        let values: Vec<_> = if format.contains(',') {
+                            values
+                                .iter()
+                                .map(|opt| opt.as_ref().map(|str| str.replace(',', "")))
+                                .collect()
+                        } else {
+                            values
+                                .iter()
+                                .map(|opt| opt.map(std::string::ToString::to_string))
+                                .collect()
+                        };
+
+                        Arc::new(StringArray::from(values))
+                    }
+                    None => array.clone(),
+                };
+
+                cast_with_options(
+                    &array,
+                    &DataType::Decimal128(*precision, *scale),
                     &CastOptions {
                         safe: self.r#try,
-                        format_options: FormatOptions::default(),
+                        format_options,
                     },
-                )?;
-                let array = array.as_any().downcast_ref::<Decimal128Array>().unwrap();
-                result.append_array(array);
+                )?
             }
             DataType::Int8
             | DataType::Int16
@@ -205,23 +260,17 @@ impl ScalarUDFImpl for ToDecimalFunc {
             | DataType::UInt32
             | DataType::UInt64
             | DataType::Float32
-            | DataType::Float64 => {
-                let array = cast_with_options(
-                    array,
-                    &DataType::Decimal128(*precission, *scale),
-                    &CastOptions {
-                        safe: self.r#try,
-                        format_options: FormatOptions::default(),
-                    },
-                )?;
-                let array = array.as_any().downcast_ref::<Decimal128Array>().unwrap();
-                result.append_array(array);
-            }
+            | DataType::Float64 => cast_with_options(
+                array,
+                &DataType::Decimal128(*precision, *scale),
+                &CastOptions {
+                    safe: self.r#try,
+                    format_options,
+                },
+            )?,
             other => return exec_err!("unexpected data type: {:?}", other),
-        }
-
-        let result = result.finish();
-        Ok(ColumnarValue::Array(Arc::new(result)))
+        };
+        Ok(ColumnarValue::Array(Arc::new(result_array)))
     }
 
     fn aliases(&self) -> &[String] {
