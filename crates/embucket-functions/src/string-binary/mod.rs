a71c30444f29076c1ebeb871f67c9224de34e9f8
Denys Tsomenko <denistsem@gmail.com>
2025-08-15 21:18:17 +0300

    Impl

--- crates/embucket-functions/src/string-binary/mod.rs
+++ crates/embucket-functions/src/string-binary/mod.rs
@@ -11,6 +11,7 @@ pub mod jarowinkler_similarity;
 pub mod length;
 pub mod lower;
 pub mod rtrimmed_length;
+pub mod randstr;
 pub mod sha2;
 pub mod split;
 pub mod strtok;
@@ -31,6 +32,7 @@ pub fn register_udfs(registry: &mut dyn FunctionRegistry) -> datafusion_common::
         jarowinkler_similarity::get_udf(),
         length::get_udf(),
         lower::get_udf(),
+        randstr::get_udf(),
         rtrimmed_length::get_udf(),
         sha2::get_udf(),
         split::get_udf(),
