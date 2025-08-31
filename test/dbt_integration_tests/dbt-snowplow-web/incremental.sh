3e9cc33066d97f06d49de4630c3757301c2af744
Oleksandr Mazur <mazur.ol.sr@gmail.com>
2025-08-31 14:57:28 +0300

    events load update

--- test/dbt_integration_tests/dbt-snowplow-web/incremental.sh
+++ test/dbt_integration_tests/dbt-snowplow-web/incremental.sh
@@ -17,9 +17,9 @@ python3 gen_events.py $num_rows
 echo "Loading events"
 python3 load_events.py events_day_before_yesterday.csv
 
-echo "Commenting out seed file copies in run_snowplow_web.sh"
-sed -i '' 's/^cp events.csv dbt-snowplow-web\/seeds\//#cp events.csv dbt-snowplow-web\/seeds\//' run_snowplow_web.sh
-sed -i '' 's/^cp seeds.yml dbt-snowplow-web\/seeds\//#cp seeds.yml dbt-snowplow-web\/seeds\//' run_snowplow_web.sh
+# echo "Commenting out seed file copies in run_snowplow_web.sh"
+# sed -i '' 's/^cp events.csv dbt-snowplow-web\/seeds\//#cp events.csv dbt-snowplow-web\/seeds\//' run_snowplow_web.sh
+# sed -i '' 's/^cp seeds.yml dbt-snowplow-web\/seeds\//#cp seeds.yml dbt-snowplow-web\/seeds\//' run_snowplow_web.sh
 
 echo "Running dbt"
 ./run_snowplow_web.sh
