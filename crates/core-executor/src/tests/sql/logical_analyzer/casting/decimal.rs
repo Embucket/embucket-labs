7fc9c1e0d188335b1ee965e6501aaaa54d5ee1f6
osipovartem <artemosipov.dev@gmail.com>
2025-09-17 14:44:49 +0300

    Fix test

--- crates/core-executor/src/tests/sql/logical_analyzer/casting/decimal.rs
+++ crates/core-executor/src/tests/sql/logical_analyzer/casting/decimal.rs
@@ -1,12 +1,8 @@
 use crate::test_query;
 
 test_query!(
-    decimal_cast,
-    "SELECT a::NUMBER FROM test",
-    setup_queries = [
-        "CREATE TABLE test (a INT32)",
-        "INSERT INTO test VALUES (50), (60)"
-    ],
+    decimal_cast_integer,
+    "SELECT column1::NUMBER as v FROM VALUES (50), (60)",
     snapshot_path = "decimal"
 );
 
