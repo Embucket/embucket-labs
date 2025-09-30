c255ad603cb7195c03402ffab177d73964c717f6
Yaroslav Litvinov <yaroslav@embucket.com>
2025-09-30 02:40:10 +0200

    shrink rows count if bytes size=(4GB - 512MB) limit exceeded

--- crates/core-history/src/entities/result_set.rs
+++ crates/core-history/src/entities/result_set.rs
@@ -6,6 +6,9 @@ use serde_json::Value;
 use snafu::{OptionExt, ResultExt};
 use std::fmt;
 
+// ResultSet exceeded limit of 4GB - 512MB
+pub const RESULT_SET_LIMIT_THRESHOULD_BYTES: usize = 4*1024*1024*1024 - 512*1024*1024;
+
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Column {
     pub name: String,
@@ -78,6 +81,69 @@ pub struct ResultSet {
     pub rows: Vec<Row>,
     pub data_format: String,
     pub schema: String,
+    // we need this before result set saved to history, do not even serialize it otherwise
+    #[serde(skip)]
+    pub batch_size_bytes: usize,
+}
+
+impl ResultSet {
+    // This takes result by reference, since it just serialize it, so can't be consumed
+    #[tracing::instrument(
+        name = "ResultSet::serialize_with_auto_limit",
+        level = "debug",
+        fields(batch_size_bytes=self.batch_size_bytes, rows_count=self.rows.len(), serialized_size, rows_count_limit),
+    )]
+    pub fn serialize_with_auto_limit(&self) -> (std::result::Result<String, serde_json::Error>, usize) {
+        let (res, count) = if self.batch_size_bytes > RESULT_SET_LIMIT_THRESHOULD_BYTES {
+            let limit_exceeded_bytes = self.batch_size_bytes - RESULT_SET_LIMIT_THRESHOULD_BYTES;
+            // how many bytes exceeded in percents (no fractions needed):
+            let exceeded_in_percents = limit_exceeded_bytes 
+                / RESULT_SET_LIMIT_THRESHOULD_BYTES * 100;
+            let shrink_on_count = if exceeded_in_percents > 50 {
+                self.rows.len() * 90 / 100 // shrink 90 % of rows
+            } else {
+                self.rows.len() * 50 / 100 // shrink 50 % of rows
+            };
+            eprintln!("
+            shrink_on_count: {shrink_on_count}, 
+            exceeded_in_percents: {exceeded_in_percents}, 
+            limit_exceeded_bytes: {limit_exceeded_bytes}");
+            let rows_count_limit: usize = if self.rows.len() > shrink_on_count {
+                self.rows.len() - shrink_on_count
+            } else {
+                self.rows.len()
+            };
+
+            (self.serialize_with_limit(rows_count_limit), rows_count_limit)
+        } else {
+            (serde_json::to_string(self), self.rows.len())
+        };
+
+        // Record the result as part of the current span.
+        tracing::Span::current()
+            .record("rows_count_limit", count)
+            .record("serialized_size", res.as_ref().map_or(0, String::len));
+
+        (res, count)
+    }
+    pub fn serialize_with_limit(&self, n_rows: usize) -> std::result::Result<String, serde_json::Error> {
+        let result_set_with_limit = LimitedResultSet {
+            columns: &self.columns,
+            rows: &self.rows[..self.rows.len().min(n_rows)],
+            data_format: &self.data_format,
+            schema: &self.schema,
+        };
+        serde_json::to_string(&result_set_with_limit)
+    }
+}
+
+// Use this struct internally for slices on rows, and serialization
+#[derive(Debug, Clone, Serialize)]
+struct LimitedResultSet<'a> {
+    pub columns: &'a [Column],
+    pub rows: &'a [Row],
+    pub data_format: &'a str,
+    pub schema: &'a str,
 }
 
 impl TryFrom<QueryRecord> for ResultSet {
